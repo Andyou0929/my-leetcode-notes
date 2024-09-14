a4be75dc165b1b8ad27ede978e7943eb1cbf432f
anchenkai <18591328376@163.com>
2024-09-14 13:26:49 +0800

    145. 二叉树的后序遍历

---
+++ 二叉树/postorderTraversal.js
@@ -0,0 +1,26 @@
+/**
+ * 145. 二叉树的后序遍历
+ * 给你一棵二叉树的根节点 root ，返回其节点值的 后序遍历 。
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+function TreeNode(val, left, right) {
+    this.val = (val===undefined ? 0 : val)
+    this.left = (left===undefined ? null : left)
+    this.right = (right===undefined ? null : right)
+}
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var postorderTraversal = function(root) {
+    return root ? [
+        ...postorderTraversal(root.left),
+        ...postorderTraversal(root.right),
+        root.val
+    ] : [];
+};
