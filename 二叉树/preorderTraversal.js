cd9ce3bc02cbd00427a1c3df03ad762c1838a50b
anchenkai <18591328376@163.com>
2024-09-14 13:27:12 +0800

    144. 二叉树的前序遍历

---
+++ 二叉树/preorderTraversal.js
@@ -0,0 +1,26 @@
+/**
+ * 144. 二叉树的前序遍历
+ * 给你二叉树的根节点 root ，返回它节点值的 前序 遍历。
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+function TreeNode(val, left, right) {
+    this.val = (val===undefined ? 0 : val)
+    this.left = (left===undefined ? null : left)
+    this.right = (right===undefined ? null : right)
+}
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var preorderTraversal = function(root) {
+    return root ? [
+        root.val,
+        ...preorderTraversal(root.left),
+        ...preorderTraversal(root.right)
+    ] : [];
+};
