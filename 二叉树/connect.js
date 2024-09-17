0c4ebd753f3b7e99490e0fcd747c1b2f092bd16f
anchenkai <18591328376@163.com>
2024-09-17 14:21:29 +0800

    116. 填充每个节点的下一个右侧节点指针 && 117. 填充每个节点的下一个右侧节点指针 II

---
+++ 二叉树/connect.js
@@ -0,0 +1,38 @@
+/**
+ * 116. 填充每个节点的下一个右侧节点指针 && 117. 填充每个节点的下一个右侧节点指针 II
+ * 给定一个 完美二叉树 ，其所有叶子节点都在同一层，每个父节点都有两个子节点。二叉树定义如下：
+ * struct Node {
+ *   int val;
+ *   Node *left;
+ *   Node *right;
+ *   Node *next;
+ * }
+ * 填充它的每个 next 指针，让这个指针指向其下一个右侧节点。如果找不到下一个右侧节点，则将 next 指针设置为 NULL。
+ * 初始状态下，所有 next 指针都被设置为 NULL。
+ * // Definition for a _Node.
+ * function _Node(val, left, right, next) {
+ *    this.val = val === undefined ? null : val;
+ *    this.left = left === undefined ? null : left;
+ *    this.right = right === undefined ? null : right;
+ *    this.next = next === undefined ? null : next;
+ * };
+ */
+
+/**
+ * @param {_Node} root
+ * @return {_Node}
+ */
+var connect = function(root) {
+    if (!root) return root;
+    const queue = [root];
+    while (queue.length) {
+        const length = queue.length;
+        for (let i = 0; i < length; i++) {
+            const node = queue.shift();
+            (i < length - 1) && (node.next = queue[0]);
+            node.left && queue.push(node.left);
+            node.right && queue.push(node.right);
+        }
+    }
+    return root;
+};
