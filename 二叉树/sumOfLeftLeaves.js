775ceb3fdba6afc343fc4ff90216b2fc7a45beac
anchenkai <18591328376@163.com>
2024-09-20 21:54:30 +0800

    404. 左叶子之和

---
+++ 二叉树/sumOfLeftLeaves.js
@@ -0,0 +1,29 @@
+/**
+ * 404. 左叶子之和
+ * 给定二叉树的根节点 root ，返回所有左叶子之和。
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+// 标记
+var sumOfLeftLeaves = function (root) {
+    if (!root) return 0;
+    let ans = 0;
+    // 查找左叶子节点
+    function getLeft(node,isLeft) {
+        if(!node) return;
+        // 判断当前节点是否为左叶子节点，isLeft为true，并且没有做叶子和右叶子
+        if(isLeft && !node.left && !node.right) ans+= node.val;
+        node.left && getLeft(node.left,true);
+        node.right && getLeft(node.right);
+    }
+    getLeft(root);
+    return ans;
+};
