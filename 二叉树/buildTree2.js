930b59d7619d17e264f9c91279ec776a888365f9
anchenkai <18591328376@163.com>
2024-09-24 20:45:16 +0800

    105. 从前序与中序遍历序列构造二叉树

--- 二叉树/buildTree2.js
+++ 二叉树/buildTree2.js
@@ -8,11 +8,6 @@
  *     this.right = (right===undefined ? null : right)
  * }
  */
-function TreeNode(val, left, right) {
-    this.val = (val === undefined ? 0 : val)
-    this.left = (left === undefined ? null : left)
-    this.right = (right === undefined ? null : right)
-}
 /**
  * @param {number[]} preorder
  * @param {number[]} inorder
